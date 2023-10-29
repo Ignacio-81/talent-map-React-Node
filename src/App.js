6a11838b15ad947e041b9d1e28175cbff82004b3
IBadella <ignacio.badella@ayi.group>
2023-10-29 11:05:13 -0300

    setting library version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
+//Creamos en Tema para la aplicacion customizando MUI
+const theme = createMuiTheme({
+    typography: {
+        "textAlign": 'left',
+    },
+    palette: {
+        primary: {
+            main: '#1473e6',
+            secondary: '#4b4b4b',
+        },
+        secondary: {
+            main: '#747474',
+        },
+    }
+
+})
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <div >
+                <header >
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+       </ThemeProvider>
+    );
 }
 
 export default App;
